234b85b7bc1503b81a1874f5e91a9738888bc41b
孙建华 <cumtsjh@163.com>
2019-03-07 16:29:21 +0800

    添加错误页面

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <title>LightCMS</title>
+    <meta name="keywords" content="LightCMS">
+    <meta name="description" content="LightCMS">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="format-detection" content="telephone=no">
+
+    <link rel="stylesheet" href="/public/vendor/layui-v2.4.5/css/layui.css" media="all">
+    <style>
+        .tc{text-align:center}
+        .error_500{padding:80px 0;}
+        .error_500 p{font-size:24px;margin:50px 0;color: #FF5722}
+        .error_500 .btn{width:160px;line-height:54px;font-size:22px;margin:0 auto;display:block;border-radius:5px;color: #FF5722}
+    </style>
+</head>
+<body>
+<div class="layui-container">
+    <div class="error_500 tc">
+        <p>访问的页面不存在</p>
+        <a href="/" class="btn">返回首页</a>
+    </div>
+</div>
+</body>
+</html>
