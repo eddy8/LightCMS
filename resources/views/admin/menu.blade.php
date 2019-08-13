2fae0dbee974e36bd6f18e9748ef1eb03dc79378
孙建华 <cumtsjh@163.com>
2019-08-13 14:39:11 +0800

    fixed: 父分类设置顶级分类不受限制

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -1,4 +1,4 @@
-<option @isset($disabledCategoryIds) @if(in_array($v['id'], $disabledCategoryIds) || (isset($disabledChildren) && !empty(array_intersect($v['path'], $disabledCategoryIds)))) disabled @endif @endisset value="{{ $v['id'] }}" @isset($model) @if($v['id'] == $model->pid) selected @endif @endisset>@for ($i = 0; $i < $v['level'] * 4; $i++) &nbsp; @endfor{{ $v['name'] }}</option>
+<option @isset($disabledCategoryIds) @if(in_array($v['id'], $disabledCategoryIds) || (isset($disabledChildren) && !empty($v['path']) && !empty(array_intersect($v['path'], $disabledCategoryIds)))) disabled @endif @endisset value="{{ $v['id'] }}" @isset($model) @if($v['id'] == $model->pid) selected @endif @endisset>@for ($i = 0; $i < $v['level'] * 4; $i++) &nbsp; @endfor{{ $v['name'] }}</option>
 @if (isset($v['children']))
     @foreach($v['children'] as $v)
             @include('admin.menu', $v)
