3862d54928c20c2cc0e6d6259b4d1639a733df10
孙建华 <cumtsjh@163.com>
2019-08-14 10:27:49 +0800

    improve: 分类选择下拉框可禁用指定分类 disabledCategoryIds

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -1,4 +1,4 @@
-<option value="{{ $v['id'] }}" @isset($model) @if($v['id'] == $model->$fieldName) selected @endif @endisset>@for ($i = 0; $i < $v['level'] * 4; $i++) &nbsp; @endfor{{ $v['name'] }}</option>
+<option @isset($disabledCategoryIds) @if(in_array($v['id'], $disabledCategoryIds)) disabled @endif @endisset value="{{ $v['id'] }}" @isset($model) @if($v['id'] == $model->$fieldName) selected @endif @endisset>@for ($i = 0; $i < $v['level'] * 4; $i++) &nbsp; @endfor{{ $v['name'] }}</option>
 @if (isset($v['children']))
     @foreach($v['children'] as $v)
             @include('admin.category', [$v, $fieldName])
