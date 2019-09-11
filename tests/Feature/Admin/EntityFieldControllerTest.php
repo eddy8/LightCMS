66c98d5884a20a933cc03365c25e0191e89264b1
孙建华 <cumtsjh@163.com>
2019-09-11 17:20:41 +0800

    add: create and edit tests of entity contents

--- tests/Feature/Admin/EntityFieldControllerTest.php
+++ tests/Feature/Admin/EntityFieldControllerTest.php
@@ -15,6 +15,7 @@ class EntityFieldControllerTest extends TestCase
 
     protected $entity;
     protected $user;
+    protected $filedName = 'title';
 
     public function setUp()
     {
@@ -28,21 +29,7 @@ class EntityFieldControllerTest extends TestCase
 
     public function testEntityFieldCanBeCreated()
     {
-        $data = [
-            'entity_id' => $this->entity->id,
-            'name' => 'title',
-            'type' => 'string',
-            'form_name' => '标题',
-            'form_type' => 'input',
-            'order' => 77,
-            'field_length' => '',
-            'field_total' => '',
-            'field_scale' => '',
-            'comment' => '',
-            'default_value' => ''
-        ];
-        $response = $this->actingAs($this->user, 'admin')
-            ->post('/admin/entityFields', $data);
+        $response = $this->createEntityField();
         $response->assertJson(['code' => 0]);
         $this->assertDatabaseHas(
             'entity_fields',
@@ -55,6 +42,44 @@ class EntityFieldControllerTest extends TestCase
                 'is_show_inline' => EntityField::SHOW_NOT_INLINE,
             ]
         );
-        $this->assertTrue(Schema::hasColumn($this->entity->table_name, $data['name']));
+        $this->assertTrue(Schema::hasColumn($this->entity->table_name, $this->filedName));
+    }
+
+    public function testEntityContentCanBeCreatedAndEdited()
+    {
+        $this->createEntityField();
+        $data = [
+            'title' => '测试标题'
+        ];
+        $response = $this->actingAs($this->user, 'admin')
+            ->post('/admin/entity/' . $this->entity->id . '/contents', $data);
+        $response->assertJson(['code' => 0]);
+
+        $data = [
+            'title' => '测试修改标题'
+        ];
+
+        $response = $this->actingAs($this->user, 'admin')
+            ->put('/admin/entity/' . $this->entity->id . '/contents/1', $data);
+        $response->assertJson(['code' => 0]);
+    }
+
+    protected function createEntityField()
+    {
+        $data = [
+            'entity_id' => $this->entity->id,
+            'name' => $this->filedName,
+            'type' => 'string',
+            'form_name' => '标题',
+            'form_type' => 'input',
+            'order' => 77,
+            'field_length' => '',
+            'field_total' => '',
+            'field_scale' => '',
+            'comment' => '',
+            'default_value' => ''
+        ];
+        return $this->actingAs($this->user, 'admin')
+            ->post('/admin/entityFields', $data);
     }
 }
