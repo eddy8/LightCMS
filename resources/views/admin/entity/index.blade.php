34c5404bb9ff351574048cb10a1e15336eae8e4a
eddy8 <cumtsjh@163.com>
2019-11-01 11:47:00 +0800

    add: 模型删除功能

--- resources/views/admin/entity/index.blade.php
+++ resources/views/admin/entity/index.blade.php
@@ -12,7 +12,7 @@
                     @include('admin.listHead', ['data' => App\Model\Admin\Entity::$listField])
                     <th lay-data="{field:'created_at'}">添加时间</th>
                     <th lay-data="{field:'updated_at'}">更新时间</th>
-                    <th lay-data="{width:200, templet:'#action'}">操作</th>
+                    <th lay-data="{width:300, templet:'#action'}">操作</th>
                 </tr>
                 </thead>
             </table>
@@ -22,6 +22,7 @@
 <script type="text/html" id="action">
     <a href="<% d.editUrl %>" class="layui-table-link" title="编辑"><i class="layui-icon layui-icon-edit"></i></a>
     <%#  if(d.enable_comment === {{ App\Model\Admin\Entity::COMMENT_ENABLE }}){ %> <a href="<% d.commentListUrl %>" class="layui-table-link" title="评论列表" style="margin-left: 5px"><i class="layui-icon layui-icon-reply-fill"></i></a> <%#  } %>
+    <a href="javascript:;" class="layui-table-link" title="删除" style="margin-left: 10px" onclick="deleteEntity('<% d.deleteUrl %>')"><i class="layui-icon layui-icon-delete"></i></a>
     <a href="<% d.fieldUrl %>" class="layui-table-link" title="字段管理" style="margin-left: 5px">字段管理</a>
     <a href="<% d.contentUrl %>" class="layui-table-link" title="字段管理" style="margin-left: 5px">内容管理</a>
 </script>
@@ -40,25 +41,31 @@
             range: '~'
         });
 
-        function deleteMenu (url) {
-            layer.confirm('确定删除？', function(index){
-                $.ajax({
-                    url: url,
-                    data: {'_method': 'DELETE'},
-                    success: function (result) {
-                        if (result.code !== 0) {
-                            layer.msg(result.msg, {shift: 6});
-                            return false;
+        function deleteEntity (url) {
+            layer.confirm('删除模型将删除模型相关的所有数据（模型、模型字段、模型分类、模型内容等），请谨慎操作！确定要删除？', function(index){
+                layer.prompt({
+                    formType: 1,
+                    title: '请输入登录密码',
+                }, function(value, index, elem){
+                    $.ajax({
+                        url: url,
+                        data: {'_method': 'DELETE', 'password': value},
+                        success: function (result) {
+                            if (result.code !== 0) {
+                                layer.msg(result.msg, {shift: 6});
+                                return false;
+                            }
+                            layer.msg(result.msg, {icon: 1}, function () {
+                                if (result.reload) {
+                                    location.reload();
+                                }
+                                if (result.redirect) {
+                                    location.href = '{!! url()->previous() !!}';
+                                }
+                            });
                         }
-                        layer.msg(result.msg, {icon: 1}, function () {
-                            if (result.reload) {
-                                location.reload();
-                            }
-                            if (result.redirect) {
-                                location.href = '{!! url()->previous() !!}';
-                            }
-                        });
-                    }
+                    });
+                    layer.close(index);
                 });
 
                 layer.close(index);
