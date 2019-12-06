ba0c7995ec4aa0e02170bfecb4ee227651c56706
eddy8 <cumtsjh@163.com>
2019-12-06 09:08:00 +0800

    add: support webp

--- config/ueditor.php
+++ config/ueditor.php
@@ -7,7 +7,7 @@ return [
     "imageActionName" => "uploadimage", /* 执行上传图片的action名称 */
     "imageFieldName" => "file", /* 提交的图片表单名称 */
     "imageMaxSize" => 2048000, /* 上传大小限制，单位B */
-    "imageAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp"], /* 上传图片格式显示 */
+    "imageAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp"], /* 上传图片格式显示 */
     "imageCompressEnable" => true, /* 是否压缩图片,默认是true */
     "imageCompressBorder" => 1600, /* 图片压缩最长边限制 */
     "imageInsertAlign" => "none", /* 插入的图片浮动方式 */
@@ -47,7 +47,7 @@ return [
     "catcherPathFormat" => "/ueditor/php/upload/image/{yyyy}{mm}{dd}/{time}{rand =>6}", /* 上传保存路径,可以自定义保存路径和文件名格式 */
     "catcherUrlPrefix" => "", /* 图片访问路径前缀 */
     "catcherMaxSize" => 2048000, /* 上传大小限制，单位B */
-    "catcherAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp"], /* 抓取图片格式显示 */
+    "catcherAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp"], /* 抓取图片格式显示 */
 
     /* 上传视频配置 */
     "videoActionName" => "uploadvideo", /* 执行上传视频的action名称 */
@@ -66,7 +66,7 @@ return [
     "fileUrlPrefix" => "", /* 文件访问路径前缀 */
     "fileMaxSize" => 51200000, /* 上传大小限制，单位B，默认50MB */
     "fileAllowFiles" => [
-        ".png", ".jpg", ".jpeg", ".gif", ".bmp",
+        ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp",
         ".flv", ".swf", ".mkv", ".avi", ".rm", ".rmvb", ".mpeg", ".mpg",
         ".ogg", ".ogv", ".mov", ".wmv", ".mp4", ".webm", ".mp3", ".wav", ".mid",
         ".rar", ".zip", ".tar", ".gz", ".7z", ".bz2", ".cab", ".iso",
@@ -79,7 +79,7 @@ return [
     "imageManagerListSize" => 20, /* 每次列出文件数量 */
     "imageManagerUrlPrefix" => "", /* 图片访问路径前缀 */
     "imageManagerInsertAlign" => "none", /* 插入的图片浮动方式 */
-    "imageManagerAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp"], /* 列出的文件类型 */
+    "imageManagerAllowFiles" => [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp"], /* 列出的文件类型 */
 
     /* 列出指定目录下的文件 */
     "fileManagerActionName" => "listfile", /* 执行文件管理的action名称 */
@@ -87,7 +87,7 @@ return [
     "fileManagerUrlPrefix" => "", /* 文件访问路径前缀 */
     "fileManagerListSize" => 20, /* 每次列出文件数量 */
     "fileManagerAllowFiles" => [
-        ".png", ".jpg", ".jpeg", ".gif", ".bmp",
+        ".png", ".jpg", ".jpeg", ".gif", ".bmp", ".webp",
         ".flv", ".swf", ".mkv", ".avi", ".rm", ".rmvb", ".mpeg", ".mpg",
         ".ogg", ".ogv", ".mov", ".wmv", ".mp4", ".webm", ".mp3", ".wav", ".mid",
         ".rar", ".zip", ".tar", ".gz", ".7z", ".bz2", ".cab", ".iso",
