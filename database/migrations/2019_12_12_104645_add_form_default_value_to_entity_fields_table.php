bfa39ca05152965f1d8e4cacc6ac6b833f1b4efa
eddy8 <cumtsjh@163.com>
2019-12-12 11:39:40 +0800

    add: 可对字段设置表单默认值

---
+++ database/migrations/2019_12_12_104645_add_form_default_value_to_entity_fields_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFormDefaultValueToEntityFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('entity_fields', function (Blueprint $table) {
+            $table->string('form_default_value')->default('')->comment('字段表单默认值');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('entity_fields', function (Blueprint $table) {
+            $table->dropColumn('votes');
+        });
+    }
+}
