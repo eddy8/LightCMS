c004b936d233fb618fb422e6b7bf80636b4bd547
eddy8 <cumtsjh@163.com>
2019-12-26 11:29:08 +0800

    improve: 前端处理非200响应

--- public/public/js/member.js
+++ public/public/js/member.js
@@ -17,8 +17,20 @@ var ajax_options = {
                 layer.msg(parse.errors[key][0], {shift: 6});
             }
             return false;
+        } else if (resp.status === 404) {
+            layer.msg('资源不存在', {icon: 5});
+            return false;
         } else if (resp.status === 401) {
             layer.msg('请先登录', {shift: 6});
+            return false;
+        } else if (resp.status === 429) {
+            layer.msg('访问过于频繁，请稍后再试', {shift: 6});
+            return false;
+        } else if (resp.status === 419) {
+            layer.msg('非法请求。请刷新页面后重试。', {shift: 6});
+            return false;
+        } else if (resp.status === 500) {
+            layer.msg('内部错误，请联系管理员', {shift: 6});
             return false;
         } else {
             var parse = $.parseJSON(resp.responseText);
