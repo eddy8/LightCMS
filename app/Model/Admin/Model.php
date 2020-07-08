f7473952acedffbd9505d801a6e296b0ccffa399
孙建华 <cumtsjh@163.com>
2020-07-08 09:18:46 +0800

    improve: date format

--- app/Model/Admin/Model.php
+++ app/Model/Admin/Model.php
@@ -9,9 +9,21 @@
 namespace App\Model\Admin;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use DateTimeInterface;
 
 class Model extends BaseModel
 {
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
+
     /**
      * 搜索字段
      *
