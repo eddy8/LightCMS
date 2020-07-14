4832651e835f8ebe53166aa9ae07076e5f4e9d0d
eddy8 <cumtsjh@163.com>
2020-07-14 17:08:05 +0800

    fix: search field

--- resources/views/admin/searchField.blade.php
+++ resources/views/admin/searchField.blade.php
@@ -20,34 +20,43 @@
             </div>
         </div>
     @elseif(is_array($v))
-        @if($v['showType'] === 'select')
+        @isset($v['showType'])
+            @if($v['showType'] === 'select')
+                <div class="layui-inline">
+                    <label class="layui-form-label">{{ $v['title'] }}</label>
+                    <div class="layui-input-inline">
+                        <select name="{{ $k }}">
+                            <option value="" @if(!request()->has($k)) selected @endif>请选择</option>
+                            @foreach($v['enums'] as $ik => $iv)
+                                <option value="{{ $ik }}" @if(request()->has($k) && request()->get($k) !== "" && request()->get($k) == $ik) selected @endif>{{ $iv }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+            @elseif($v['showType'] === 'datetime')
+                <div class="layui-inline">
+                    <label class="layui-form-label">{{ $v['title'] }}</label>
+                    <div class="layui-input-inline">
+                        <input type="text" name="{{ $k }}" class="layui-input" id="{{ $k }}" value="{{ request()->get($k) }}">
+                    </div>
+                </div>
+                <script>
+                    addLoadEvent(function () {
+                        var laydate = layui.laydate;
+                        laydate.render({
+                            elem: '#{{ $k }}',
+                            range: '~'
+                        });
+                    })
+                </script>
+            @endif
+        @else
             <div class="layui-inline">
                 <label class="layui-form-label">{{ $v['title'] }}</label>
                 <div class="layui-input-inline">
-                    <select name="{{ $k }}">
-                        <option value="" @if(!request()->has($k)) selected @endif>请选择</option>
-                        @foreach($v['enums'] as $ik => $iv)
-                            <option value="{{ $ik }}" @if(request()->has($k) && request()->get($k) !== "" && request()->get($k) == $ik) selected @endif>{{ $iv }}</option>
-                        @endforeach
-                    </select>
+                    <input type="text" name="{{ $k }}" autocomplete="off" class="layui-input" value="{{ request()->get($k) }}">
                 </div>
             </div>
-        @elseif($v['showType'] === 'datetime')
-            <div class="layui-inline">
-                <label class="layui-form-label">{{ $v['title'] }}</label>
-                <div class="layui-input-inline">
-                    <input type="text" name="{{ $k }}" class="layui-input" id="{{ $k }}" value="{{ request()->get($k) }}">
-                </div>
-            </div>
-            <script>
-                addLoadEvent(function () {
-                    var laydate = layui.laydate;
-                    laydate.render({
-                        elem: '#{{ $k }}',
-                        range: '~'
-                    });
-                })
-            </script>
-        @endif
+        @endisset
     @endif
 @endforeach
