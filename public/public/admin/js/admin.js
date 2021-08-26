045cf6babf7a606290cbdd34f23c52b06acc8163
孙建华 <cumtsjh@163.com>
2021-08-26 15:10:20 +0800

    improve: 401 message tips

--- public/public/admin/js/admin.js
+++ public/public/admin/js/admin.js
@@ -21,7 +21,11 @@ var ajax_options = {
             layer.msg('资源不存在', {icon: 5});
             return false;
         } else if (resp.status === 401) {
-            layer.msg('权限不足或帐户未登录', {shift: 6});
+            var d = $.parseJSON(resp.responseText), tips = "权限不足或帐户未登录";
+            if (d && d.msg) {
+                tips = d.msg;
+            }
+            layer.msg(tips, {shift: 6});
             return false;
         } else if (resp.status === 429) {
             layer.msg('访问过于频繁，请稍后再试', {shift: 6});
